2202f5abc2ea7687531b7b30c63fec55103fa063
tada5hi <peter.placzek1996@gmail.com>
2023-01-26 16:38:31 +0100

    build: updated jest-config transformer to array syntax

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     testEnvironment: 'node',
     transform: {
-        "^.+\\.tsx?$": "ts-jest"
+        "^.+\\.tsx?$": ["ts-jest"]
     },
     moduleFileExtensions: [
         "ts",
